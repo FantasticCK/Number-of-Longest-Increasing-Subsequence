d4c3a0ea841b1deefd1013cc4a9169c3824e2988
CK <kainingc@usc.edu>
2020-01-13 11:52:20 -0800

    first commit

---
+++ src/com/CK/Main.java
@@ -0,0 +1,117 @@
+package com.CK;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
+
+class Solution {
+    public int findNumberOfLIS(int[] nums) {
+        if (nums.length == 0)
+            return 0;
+        int global = 1, maxLen = 0, res = 0;
+        int[] local = new int[nums.length], count = new int[nums.length];
+        local[0] = 1;
+        count[0] = 1;
+        for (int i = 0; i < nums.length; i++) {
+            local[i] = 1;
+            count[i] = 1;
+            for (int j = i - 1; j >= 0; j--) {
+                if (nums[j] < nums[i]) {
+                    if(local[i] == local[j] + 1)
+                        count[i] += count[j];
+                    if (local[i] < local[j] + 1) {
+                        local[i] = local[j] + 1;
+                        count[i] = count[j];
+                    }
+                }
+            }
+
+            if (maxLen < local[i]) {
+                maxLen = local[i];
+                res = count[i];
+            } else if (maxLen == local[i]) {
+                res += count[i];
+            }
+        }
+        return res;
+    }
+}
+
+class Solution {
+    public Value merge(Value v1, Value v2) {
+        if (v1.length == v2.length) {
+            if (v1.length == 0) return new Value(0, 1);
+            return new Value(v1.length, v1.count + v2.count);
+        }
+        return v1.length > v2.length ? v1 : v2;
+    }
+
+    public void insert(Node node, int key, Value val) {
+        if (node.range_left == node.range_right) {
+            node.val = merge(val, node.val);
+            return;
+        } else if (key <= node.getRangeMid()) {
+            insert(node.getLeft(), key, val);
+        } else {
+            insert(node.getRight(), key, val);
+        }
+        node.val = merge(node.getLeft().val, node.getRight().val);
+    }
+
+    public Value query(Node node, int key) {
+        if (node.range_right <= key) return node.val;
+        else if (node.range_left > key) return new Value(0, 1);
+        else return merge(query(node.getLeft(), key), query(node.getRight(), key));
+    }
+
+    public int findNumberOfLIS(int[] nums) {
+        if (nums.length == 0) return 0;
+        int min = nums[0], max = nums[0];
+        for (int num: nums) {
+            min = Math.min(min, num);
+            max = Math.max(max, num);
+        }
+        Node root = new Node(min, max);
+        for (int num: nums) {
+            Value v = query(root, num-1);
+            insert(root, num, new Value(v.length + 1, v.count));
+        }
+        return root.val.count;
+    }
+}
+
+class Node {
+    int range_left, range_right;
+    Node left, right;
+    Value val;
+    public Node(int start, int end) {
+        range_left = start;
+        range_right = end;
+        left = null;
+        right = null;
+        val = new Value(0, 1);
+    }
+    public int getRangeMid() {
+        return range_left + (range_right - range_left) / 2;
+    }
+    public Node getLeft() {
+        if (left == null) left = new Node(range_left, getRangeMid());
+        return left;
+    }
+    public Node getRight() {
+        if (right == null) right = new Node(getRangeMid() + 1, range_right);
+        return right;
+    }
+}
+
+class Value {
+    int length;
+    int count;
+    public Value(int len, int ct) {
+        length = len;
+        count = ct;
+    }
+}
